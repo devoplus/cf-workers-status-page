2532df465332dead1fe0dcbdd06009e8da14f8f7
devoplus <142236866+devoplus@users.noreply.github.com>
2023-08-16 15:54:33 +0300

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -18,19 +18,21 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
   }
 
   return (
-    <div className={`card mb-4 font-semibold ${classes[color]}`}>
-      <div className="flex flex-row justify-between items-center">
-        <div>{text}</div>
-        {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
-          <div className="text-xs font-light">
-            checked{' '}
-            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
-            {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
-          </div>
-        )}
+    <div>
+      <div className={`card mb-4 font-semibold ${classes[color]}`}>
+        <div className="flex flex-row justify-between items-center">
+          <div>{text}</div>
+          {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
+            <div className="text-xs font-light">
+              checked{' '}
+              {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
+              ago (from{' '}
+              {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
+            </div>
+          )}
+        </div>
       </div>
+      <div className="card-uptime"></div>
     </div>
-    <div className="card-uptime"></div>
   )
 }
