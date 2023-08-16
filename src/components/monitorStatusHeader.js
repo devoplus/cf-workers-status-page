0c0dd7db2ed64bf6c2d181ad76ffceabf5e86b59
devoplus <142236866+devoplus@users.noreply.github.com>
2023-08-16 15:27:15 +0300

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -31,5 +31,10 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         )}
       </div>
     </div>
+    <div class="card mb-4 font-semibold bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-200 border-gray-300 dark:border-gray-600">
+       <div class="flex flex-row justify-between items-center">
+           <div>Yearly Total Uptime: <span id="spnYearlyUptime">Loading...</span></div>
+       </div>
+    </div>
   )
 }
